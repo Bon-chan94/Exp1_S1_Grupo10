42a36f4235fd805a2d81476263d58e9ec1920537
alan figueroa <ala.figueroar@duocuc.cl>
2025-05-21 22:55:42 -0400

    grupo 10

---
+++ cuentaCorriente.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+// Clase CuentaCorriente
+class CuentaCorriente {
+    private String numeroCuenta;
+    private int saldo;
+
+    public CuentaCorriente(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = 0; // Inicia en 0
+    }
+
+    public boolean depositar(int monto) {
+        if (monto > 0) {
+            saldo += monto;
+            System.out.println("Depósito realizado con éxito.");
+            return true;
+        } else {
+            System.out.println("Monto inválido. Debe ser mayor a 0.");
+            return false;
+        }
+    }
+
+    public boolean girar(int monto) {
+        if (monto <= 0) {
+            System.out.println("Monto inválido. Debe ser mayor a 0.");
+            return false;
+        } else if (monto > saldo) {
+            System.out.println("Fondos insuficientes.");
+            return false;
+        } else {
+            saldo -= monto;
+            System.out.println("Giro realizado con éxito.");
+            return true;
+        }
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+}
